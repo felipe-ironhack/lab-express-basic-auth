fa6c549784652e2ab662239f30cc3d935a45f1a7
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-01-12 19:08:39 +0100

    add signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,33 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-router.get('/signup', (req, res) => {
-  res.render('auth/signup')
-})
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User.model');
 
-module.exports = router
+const saltRounds = 12;
+
+// Display the Signup form
+router.get('/signup', (req, res) => res.render('auth/signup'));
+
+// Process Signup info
+router.post('/signup', async (req, res, next) => {
+	const { username, pw } = req.body;
+	try {
+		const userFromDB = await User.findOne({ username });
+
+		if (userFromDB) {
+      return res.render('auth/signup', { errorMessage: 'user already exists'})
+    }
+
+    const passwordHash = await bcryptjs.hash(pw, saltRounds);
+    
+    const newUser = await User.create({username, password: passwordHash}) 
+
+    console.log(newUser)
+    res.render('auth/success')
+	} catch (err) {
+		console.log(`Error creating the user in the DB ${err}`);
+		next(err);
+	}
+});
+
+module.exports = router;
